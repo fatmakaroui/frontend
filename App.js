613c24974478820929fb2787378f1e0dfc0ef503
fatma karoui <fatmakaroui1@gmail.com>
2020-08-09 13:55:26 +0100

    Syncro storage yemchi

---
+++ App.js
@@ -0,0 +1,26 @@
+import React,{useEffect,useState} from 'react';
+import { Provider } from 'react-native-paper';
+import App from './src';
+import { theme } from './src/core/theme';
+import AsyncStorage from '@react-native-community/async-storage';
+const Main = () => {
+  const [isloggedin,setLogged] = useState(null)
+  const detectLogin= async ()=>{
+     const token = await AsyncStorage.getItem('token')
+     if(token){
+         setLogged(true)
+     }else{
+         setLogged(false)
+     }
+  }
+ useEffect(()=>{
+    detectLogin()
+ },[])
+
+
+  return(<Provider theme={theme}>
+    <App />
+  </Provider>
+)};
+
+export default Main;
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,52 @@
+import React, { memo ,useEffect, useState } from 'react';
+import Background from '../components/Background';
+import Logo from '../components/Logo';
+import Header from '../components/Header';
+import Paragraph from '../components/Paragraph';
+import Button from '../components/Button';
+import AsyncStorage from '@react-native-community/async-storage';
+import { baseUrl } from '../shared/baseUrl';
+
+const Dashboard = ({ navigation,props }) => {
+  const [email,setEmail] = useState("loading")
+  const Boiler = async ()=>{
+     const token = await AsyncStorage.getItem("token")
+   fetch(baseUrl,{
+   headers:new Headers({
+     Authorization:"Bearer "+token
+    
+   })
+   }).then(res=>res.json())
+   .then(data=>{
+     console.log(data)
+     setEmail(data.email)
+   }
+   )
+  }
+useEffect(()=>{
+  Boiler()
+},[])
+
+  const logout =(props)=>{
+     AsyncStorage.removeItem("token").then(()=>{
+      navigation.navigate('HomeScreen')
+     })
+  }
+
+
+
+  return(<Background>
+    <Logo />
+    <Header>Let’s start</Header>
+    <Paragraph>
+      Your amazing app starts here. Open you favourite code editor and start
+      editing this project.
+    </Paragraph>
+    <Button mode="outlined" onPress={() => logout()}>
+      Logout
+    </Button>
+  </Background>)
+
+};
+
+export default memo(Dashboard);
